0af2384a131aef9e1d814ffa002cfade2f9721c7
RodrigoLimaM <digo100200@gmail.com>
2020-10-11 12:29:29 -0300

    initializing project

---
+++ src/main/java/br/com/salesmanager/tracking/SalesManagerTrackingApplication.java
@@ -0,0 +1,15 @@
+package br.com.salesmanager.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@EnableMongoAuditing
+@SpringBootApplication
+public class SalesManagerTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/salesmanager/tracking/SalesManagerTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salesmanager.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagerTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
