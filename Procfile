191dfb37cf97cf887f43fcab6913ec68bd37f460
RodrigoLimaM <digo100200@gmail.com>
2020-11-20 21:04:21 -0300

    creating procfile and downgrading to java 8

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/sales-manager-tracking-0.0.1-SNAPSHOT.jar
--- src/main/java/br/com/salesmanager/tracking/service/TrackingService.java
+++ src/main/java/br/com/salesmanager/tracking/service/TrackingService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -55,7 +56,7 @@ public class TrackingService {
     }
 
     public OrderTracking incrementStep(OrderTracking orderTracking, OrderStatus status) {
-        var steps = orderTracking.getSteps();
+        List<Step> steps = orderTracking.getSteps();
         steps.add(Step.builder()
                 .orderStatus(status)
                 .date(LocalDateTime.now())
